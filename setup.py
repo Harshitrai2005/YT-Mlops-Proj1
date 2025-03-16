3fecfadc45e01a4ce9ddf49de9201b3bdc73ee39
Harshitrai2005 <harshitrai2532005@gmail.com>
2025-03-16 20:31:11 +0530

    project structure made and venv sent

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Harshit_Rai",
+    author_email="harshitrai2532005@gmail.com",
+    packages=find_packages()
+)
